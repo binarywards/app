f74f5fa23263c4f122a659ed17f2e4c8fb39b7cf
Billcountry <billcountrymwaniki@gmail.com>
2018-02-08 13:50:27 +0300

    Africa's talking gateway implementation using requests

---
+++ logic/at_gateway.py
@@ -0,0 +1,69 @@
+class utilities:
+    http_get = "GET"
+    http_post = "POST"
+    http_put = "PUT"
+    http_delete = "DELETE"
+
+    def py_jax(self, url, method="GET", data_type="json", headers=None, **params):
+        import requests
+        if headers is None:
+            headers = {}
+        request = None
+        response = None
+        if method == "POST":
+            request = requests.post(url=url, data=params, headers=headers)
+        elif method == "PUT":
+            request = requests.put(url=url, data=params, headers=headers)
+        elif method == "DELETE":
+            request = requests.delete(url=url, **params, headers=headers)
+        else:
+            request = requests.get(url=url, params=params, headers=headers)
+        if data_type == "json":
+            response = request.json()
+        else:
+            response = request.text
+        return response
+
+
+class urls:
+    send_message = "http://api.africastalking.com/version1/messaging"
+    send_airtime = "http://api.africastalking.com/version1/airtime/send"
+
+
+class at_gateway:
+    default_headers = {
+        "accept": "application/json"
+    }
+    username = None
+    utils = utilities()
+
+    def __init__(self, username=None, api_key=None):
+        if username is None or api_key is None:
+            raise Exception('username and api_key required')
+        self.default_headers["Apikey"] = api_key
+        self.username = username
+
+    def send_message(self, to, message, sender='20414'):
+        if type(to) is list:
+            recipients = to[0]
+            to.pop(0)
+            for recipient in to:
+                recipients += "," + recipient
+        else:
+            recipients = to
+        return self.utils.py_jax(url=urls.send_message, method=utilities.http_post, headers=self.default_headers,
+                                 username=self.username, to=recipients, message=message, **{'from': sender})
+
+    def ussd_response(self, cont=False, message="Bye..."):
+        if cont:
+            message = "CON " + message
+        else:
+            message = "END " + message
+        return message
+
+    def buy_airtime(self, recipients=[{"phoneNumber": "0728824727", "amount": "KES 20"}]):
+        #convert recipients to JSON
+        import json
+        recipients = json.dumps(recipients, indent=4, separators=(',', ': '))
+        return self.utils.py_jax(url=urls.send_airtime, method=utilities.http_post, headers=self.default_headers,
+                                 username=self.username, recipients=recipients)
