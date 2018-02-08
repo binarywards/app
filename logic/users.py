cc5d9a5d037cda85e8c5994773490a4f4a2875a8
Billcountry <billcountrymwaniki@gmail.com>
2018-02-08 14:16:57 +0300

    Added users.py

---
+++ logic/users.py
@@ -0,0 +1,18 @@
+class users:
+    def register(self, phone, password, username=None, email=None):
+        pass
+
+    def login(self):
+        pass
+
+    def update_details(self):
+        pass
+
+    def logout(self):
+        pass
+
+    def single_auth(self):
+        pass
+
+    def user_exists(self, phone):
+        pass
