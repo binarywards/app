e0a1a9ec922278b183f018dcbfbe1325e2d66438
Billcountry <billcountrymwaniki@gmail.com>
2018-02-09 19:18:20 +0300

    Added function to buy airtime for a single number

--- logic/at_gateway.py
+++ logic/at_gateway.py
@@ -19,7 +19,7 @@ class utilities:
         else:
             request = requests.get(url=url, params=params, headers=headers)
         if data_type == "json":
-            response = request.json()
+            response = request.text
         else:
             response = request.text
         return response
@@ -28,6 +28,7 @@ class utilities:
 class urls:
     send_message = "http://api.africastalking.com/version1/messaging"
     send_airtime = "http://api.africastalking.com/version1/airtime/send"
+    payment_b2c = "https://payments.africastalking.com/mobile/b2c/request"
 
 
 class at_gateway:
@@ -43,7 +44,7 @@ class at_gateway:
         self.default_headers["Apikey"] = api_key
         self.username = username
 
-    def send_message(self, to, message, sender='20414'):
+    def send_message(self, to, message):
         if type(to) is list:
             recipients = to[0]
             to.pop(0)
@@ -52,7 +53,7 @@ class at_gateway:
         else:
             recipients = to
         return self.utils.py_jax(url=urls.send_message, method=utilities.http_post, headers=self.default_headers,
-                                 username=self.username, to=recipients, message=message, **{'from': sender})
+                                 username=self.username, to=recipients, message=message)
 
     def ussd_response(self, cont=False, message="Bye..."):
         if cont:
@@ -61,9 +62,22 @@ class at_gateway:
             message = "END " + message
         return message
 
+    def buy_airtime_single(self, phoneNumber, amount):
+        #convert recipients to JSON
+        import json
+        recipients = [{"phoneNumber": phoneNumber, "amount": "KES "+str(amount)}]
+        recipients = json.dumps(recipients, indent=4, separators=(',', ': '))
+        return self.utils.py_jax(url=urls.send_airtime, method=utilities.http_post, headers=self.default_headers,
+                                 username=self.username, recipients=recipients)
+
     def buy_airtime(self, recipients=[{"phoneNumber": "0728824727", "amount": "KES 20"}]):
         #convert recipients to JSON
         import json
         recipients = json.dumps(recipients, indent=4, separators=(',', ': '))
         return self.utils.py_jax(url=urls.send_airtime, method=utilities.http_post, headers=self.default_headers,
                                  username=self.username, recipients=recipients)
+
+    def send_cash_single_mpesa_b2c(self, name, phone, amount):
+        headers = self.default_headers
+        headers["Content-Type"] = "application/json"
+        # will have to finish this later
